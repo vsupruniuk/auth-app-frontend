b5c07f576221097e5f3d41a0c8da8a9d43e18973
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2023-02-01 09:49:36 +0200

    Test

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './components/AuthContext';
 
 import App from './App';
